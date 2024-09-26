70c14fec38e18cd409bf9fe78f44ca9d5e0aab61
xSteve <117078465+HieuCoser@users.noreply.github.com>
2024-09-27 02:41:02 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
+// scripts.js
+
+// Cuộn mượt khi nhấp vào liên kết trong menu (nếu bạn có menu)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
 
